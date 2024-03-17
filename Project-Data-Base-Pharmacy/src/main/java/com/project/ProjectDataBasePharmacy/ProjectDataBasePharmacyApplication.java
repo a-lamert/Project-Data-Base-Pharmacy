d4c4a6320a66028e7cd2cc66061e4bd078b2720e
A Lamert <andrei14la@hotmail.com>
2024-03-18 00:19:00 +0100

    git

---
+++ Project-Data-Base-Pharmacy/src/main/java/com/project/ProjectDataBasePharmacy/ProjectDataBasePharmacyApplication.java
@@ -0,0 +1,13 @@
+package com.project.ProjectDataBasePharmacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDataBasePharmacyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectDataBasePharmacyApplication.class, args);
+	}
+
+}
