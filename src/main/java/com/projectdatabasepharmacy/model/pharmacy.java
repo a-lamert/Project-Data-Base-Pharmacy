c59748f2c009bc3aee9211ff95eb8952e086423f
A Lamert <andrei14la@hotmail.com>
2024-03-19 00:20:54 +0100

    git checkout clean_up

---
+++ src/main/java/com/projectdatabasepharmacy/model/pharmacy.java
@@ -0,0 +1,35 @@
+package com.projectdatabasepharmacy.model;
+
+public class pharmacy {
+    private int id;
+    private String name;
+    private String discount;
+
+    public pharmacy() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(String discount) {
+        this.discount = discount;
+    }
+}
+
